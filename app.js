2f527106fa0e4a893414088a9b9b93589f3370bf
Karin <karin.j.alvarez@gmail.com>
2014-02-20 16:00:58 -0700

    Second commit

--- app.js
+++ app.js
@@ -4,11 +4,9 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
-
+var submissionPageController = require('./controllers/submissionpagecontroller.js')
 var app = express();
 
 // all environments
@@ -28,8 +26,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', submissionPageController.index);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ models/submissiondata.js
@@ -0,0 +1,6 @@
+var submission = function(creator, video URL, title, description) {
+	this.creator = creator;
+	this.url = URL;
+	this.title=title;
+	this.description=description;
+}
